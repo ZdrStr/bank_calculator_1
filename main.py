f6530630e6f7475f30788ee5fc5b6fdc9f36d5d3
Zdravko Stoyanov <>
2021-04-26 08:13:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import regex
+import random
+
+
+
+while True:
+  choice = int(input("1. Create an account\n2. Log into account\n0. Exit:\n"))
+  if choice == 1:
+    print("Your card has been created\n Your card number:\n 4000004938320895\n")
+    
